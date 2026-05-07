52b6fa4c85806730ee910cfde01bae266d8297e6
leite-1309 <leite.lucas1309@gmail.com>
2026-05-07 12:09:04 -0300

    feat : Realização da lição de casa;

--- js/script.js
+++ js/script.js
@@ -21,56 +21,62 @@
 
 
 //Tipos de dados:
-const nome = "Alê";
-console.log(typeof nome);
-const idade = 20;
-console.log(typeof idade);
-const numeroDecimal = 20.345678910111;
-console.log(typeof numeroDecimal);
-const estaAprovado = true;
-console.log(typeof estaAprovado);
-const sobrenome = undefined;
-console.log(typeof sobrenome);
-let naoDeclarada;
-console.log(typeof naoDeclarada);
-const corSelecionada = null;
-console.log(typeof corSelecionada);
-const objeto = {};
-console.log(typeof objeto);
-const array = [];
-console.log(typeof array);
-const objeto2 = {nome: "Alê", sobrenome: "C. de Jesus"};
-console.log(typeof objeto2);
-const array2 = ["Alê", "C. de Jesus", 20, true];
-console.log(typeof array2);
-const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const nome = "Alê";
+// console.log(typeof nome);
+// const idade = 20;
+// console.log(typeof idade);
+// const numeroDecimal = 20.345678910111;
+// console.log(typeof numeroDecimal);
+// const estaAprovado = true;
+// console.log(typeof estaAprovado);
+// const sobrenome = undefined;
+// console.log(typeof sobrenome);
+// let naoDeclarada;
+// console.log(typeof naoDeclarada);
+// const corSelecionada = null;
+// console.log(typeof corSelecionada);
+// const objeto = {};
+// console.log(typeof objeto);
+// const array = [];
+// console.log(typeof array);
+// const objeto2 = {nome: "Alê", sobrenome: "C. de Jesus"};
+// console.log(typeof objeto2);
+// const array2 = ["Alê", "C. de Jesus", 20, true];
+// console.log(typeof array2);
+// const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//Tipagem no JS é dinâmica
-let variavel;
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// //Tipagem no JS é dinâmica
+// let variavel;
+// console.log("Valor: " + variavel);
+// console.log("Tipo: " + typeof variavel);
 
-variavel = "Alê";
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// variavel = "Alê";
+// console.log("Valor: " + variavel);
+// console.log("Tipo: " + typeof variavel);
 
-variavel = true;
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// variavel = true;
+// console.log("Valor: " + variavel);
+// console.log("Tipo: " + typeof variavel);
 
 
-//Como declaramos um Objeto em javaScript
-const objeto1 = {
-  nome: "Alê",
-  sobrenome: "C. de Jesus",
-  idade: 20,
-  estaAprovado: true
-  };
-console.log(objeto1);
-console.log(typeof objeto1);
+// //Como declaramos um Objeto em javaScript
+// const objeto1 = {
+//   nome: "Alê",
+//   sobrenome: "C. de Jesus",
+//   idade: 20,
+//   estaAprovado: true
+//   };
+// console.log(objeto1);
+// console.log(typeof objeto1);
 
 
 // Lição de Casa:
 // Crie um objeto que seja semelhante aos dados do formulário de login
 // Imprmir os dados do objeto no console.
 
+//Lição de casa:
+const cliente = {
+    Email: "email",
+    Senha: "12345"
+}
+console.log(cliente)
