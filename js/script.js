bd53921ebd9e17f5a75f2c8ac31a27aa6cdb67d7
flavio <rm571211@fiap.com.br>
2026-05-08 12:02:50 -0300

    feat : manipulação do dom e criação de objetos

--- js/script.js
+++ js/script.js
@@ -21,62 +21,108 @@
 
 
 //Tipos de dados:
-const nome = "Alê";
-console.log(typeof nome);
-const idade = 20;
-console.log(typeof idade);
-const numeroDecimal = 20.345678910111;
-console.log(typeof numeroDecimal);
-const estaAprovado = true;
-console.log(typeof estaAprovado);
-const sobrenome = undefined;
-console.log(typeof sobrenome);
-let naoDeclarada;
-console.log(typeof naoDeclarada);
-const corSelecionada = null;
-console.log(typeof corSelecionada);
-const objeto = {};
-console.log(typeof objeto);
-const array = [];
-console.log(typeof array);
-const objeto2 = {nome: "Alê", sobrenome: "C. de Jesus"};
-console.log(typeof objeto2);
-const array2 = ["Alê", "C. de Jesus", 20, true];
-console.log(typeof array2);
-const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const nome = "Alê";
+// console.log(typeof nome);
+// const idade = 20;
+// console.log(typeof idade);
+// const numeroDecimal = 20.345678910111;
+// console.log(typeof numeroDecimal);
+// const estaAprovado = true;
+// console.log(typeof estaAprovado);
+// const sobrenome = undefined;
+// console.log(typeof sobrenome);
+// let naoDeclarada;
+// console.log(typeof naoDeclarada);
+// const corSelecionada = null;
+// console.log(typeof corSelecionada);
+// const objeto = {};
+// console.log(typeof objeto);
+// const array = [];
+// console.log(typeof array);
+// const objeto2 = {nome: "Alê", sobrenome: "C. de Jesus"};
+// console.log(typeof objeto2);
+// const array2 = ["Alê", "C. de Jesus", 20, true];
+// console.log(typeof array2);
+// const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//Tipagem no JS é dinâmica
-let variavel;
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// //Tipagem no JS é dinâmica
+// let variavel;
+// console.log("Valor: " + variavel);
+// console.log("Tipo: " + typeof variavel);
 
-variavel = "Alê";
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// variavel = "Alê";
+// console.log("Valor: " + variavel);
+// console.log("Tipo: " + typeof variavel);
 
-variavel = true;
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// variavel = true;
+// console.log("Valor: " + variavel);
+// console.log("Tipo: " + typeof variavel);
 
 
-//Como declaramos um Objeto em javaScript
-const objeto1 = {
-  nome: "Alê",
-  sobrenome: "C. de Jesus",
-  idade: 20,
-  estaAprovado: true
-  };
-console.log(objeto1);
-console.log(typeof objeto1);
-
+// //Como declaramos um Objeto em javaScript
+// const objeto1 = {
+//   nome: "Alê",
+//   sobrenome: "C. de Jesus",
+//   idade: 20,
+//   estaAprovado: true
+//   };
+// console.log(objeto1);
+// console.log(typeof objeto1);
+ 
 
 // Lição de Casa:
 // Crie um objeto que seja semelhante aos dados do formulário de login
 // Imprmir os dados do objeto no console.
 
-const dadosLogin = {
-  email: "alexandre@email.com",
-  senha: "senha123"
-};
- 
-console.log(dadosLogin);
+
+//Declarando um objeto espelhado ao FORMULÁRIO DE LOGIN
+
+//Objeto Usuário:
+
+// const usuario = { 
+//   email:"email@email.com",
+//   senha:"12345",
+//   nome:"José das Couves",
+//   idade:33,
+//   avatar:"./img/avatar/img-avatar-01.jpg"
+// }
+
+// //Imprimindo os dados do objeto:
+// console.log(usuario);
+// console.table(usuario);
+
+// //Imprimindo um único dado do objeto:
+// console.log(usuario.nome);
+
+// //Alterar um único dado do objeto:
+// usuario.nome = "Antonio das Alfaces";
+// //Imprimindo um único dado do objeto:
+// console.log(usuario.nome);
+
+// let nome = "Joaquim dos Tomates";
+// //Alterar um único dado do objeto:
+// usuario.nome = nome;
+// //Imprimindo um único dado do objeto:
+// console.log(usuario.nome);
+
+// const subTitH2 = document.getElementById("teste");
+// //Imprimeindo o elemento capturado:
+// console.log(subTitH2);
+
+// //Vamos escrever no h2
+// // subTitH2.textContent = "<p>NOVO TEXTO</p>";
+// subTitH2.innerHTML = "<p>novo texto</p>";
+
+const usuario = { 
+  email:"email@email.com",
+  senha:"12345",
+  nome:"José das Couves",
+  idade:33,
+  avatar:"./img/avatar/img-avatar-01.jpg"
+}
+
+const imgAvatar = document.getElementById("avatar-user");
+
+imgAvatar.src = usuario.avatar;
+imgAvatar.width = 100;
+imgAvatar.alt = usuario.nome;
