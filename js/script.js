3afc3e745da8b2ab218d15db9f39326d0df4a077
JoaoVitor-2209 <vitorcruzrocha@gmail.com>
2026-05-08 12:03:10 -0300

    feat : Manipulação do DOM com imagem e criação de objetos..

--- js/script.js
+++ js/script.js
@@ -1,85 +1,135 @@
 
 
-// Comenterio em linha
-/*
-Comentario em bloco
-*/
+// // // Comenterio em linha
+// // /*
+// // Comentario em bloco
+// // */
 
-//Forma de declarar de variavel
-//var nome = "João"; //Deve ser evitado
-//let sobrenome = "C. de Lima";// Deve ser ultilizado
-//const idade = 19; //Constante, não pode ser alterada depois de declarada, deve ser ultilizada quando o valor não for alterado
+// // //Forma de declarar de variavel
+// // //var nome = "João"; //Deve ser evitado
+// // //let sobrenome = "C. de Lima";// Deve ser ultilizado
+// // //const idade = 19; //Constante, não pode ser alterada depois de declarada, deve ser ultilizada quando o valor não for alterado
 
-//console.log(nome);
-//console.log(sobrenome);
-//console.log(idade);      
+// // //console.log(nome);
+// // //console.log(sobrenome);
+// // //console.log(idade);      
 
-//Exemplo de diferencia entre var, let e const
+// // //Exemplo de diferencia entre var, let e const
 
-//if(true){
-    //var nome = "Ana";
-    //let sobrenome = "C. Cavalcante";
-   // const idade = 18;
-//}
-    //console.log(nome);
-    //console.log(sobrenome);
-    // console.log(idade);
+// // //if(true){
+// //     //var nome = "Ana";
+// //     //let sobrenome = "C. Cavalcante";
+// //    // const idade = 18;
+// // //}
+// //     //console.log(nome);
+// //     //console.log(sobrenome);
+// //     // console.log(idade);
     
-//Tipos de dados:
-const nome = "João";
-console.log(typeof nome);
-const idade = 19;
-console.log(typeof idade);
-const numeroDecimal = 20.345678910111;
-console.log(typeof numeroDecimal);
-const estaAprovado = true;
-console.log(typeof estaAprovado);
-const sobrenome = undefined;
-console.log(typeof sobrenome);
-let naoDeclarada;
-console.log(typeof naoDeclarada);
-const corSelecionada = null;
-console.log(typeof corSelecionada);
-const objeto = {};
-console.log(typeof objeto);
-const array = [];
-console.log(typeof array);
-const objeto2 = {nome: "Joao", sobrenome: "C. de Lima"};
-console.log(typeof objeto2);
-const array2 = ["Joao", "C. de Lima", 19, true];
-console.log(typeof array2);
-const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // //Tipos de dados:
+// // const nome = "João";
+// // console.log(typeof nome);
+// // const idade = 19;
+// // console.log(typeof idade);
+// // const numeroDecimal = 20.345678910111;
+// // console.log(typeof numeroDecimal);
+// // const estaAprovado = true;
+// // console.log(typeof estaAprovado);
+// // const sobrenome = undefined;
+// // console.log(typeof sobrenome);
+// // let naoDeclarada;
+// // console.log(typeof naoDeclarada);
+// // const corSelecionada = null;
+// // console.log(typeof corSelecionada);
+// // const objeto = {};
+// // console.log(typeof objeto);
+// // const array = [];
+// // console.log(typeof array);
+// // const objeto2 = {nome: "Joao", sobrenome: "C. de Lima"};
+// // console.log(typeof objeto2);
+// // const array2 = ["Joao", "C. de Lima", 19, true];
+// // console.log(typeof array2);
+// // const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//Tipagem no JS é dinâmica
-let variavel;
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// // //Tipagem no JS é dinâmica
+// // let variavel;
+// // console.log("Valor: " + variavel);
+// // console.log("Tipo: " + typeof variavel);
 
-variavel = "JV";
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// // variavel = "JV";
+// // console.log("Valor: " + variavel);
+// // console.log("Tipo: " + typeof variavel);
 
-variavel = true;
-console.log("Valor: " + variavel);
-console.log("Tipo: " + typeof variavel);
+// // variavel = true;
+// // console.log("Valor: " + variavel);
+// // console.log("Tipo: " + typeof variavel);
 
-//Como declaramos um Objeto em javaScript
-const objeto1 = {
-  nome: "JV",
-  sobrenome: "C. de Lima",
-  idade: 19,
-  estaAprovado: true
-  };
-console.log(objeto1);
-console.log(typeof objeto1);
+// // //Como declaramos um Objeto em javaScript
+// // const objeto1 = {
+// //   nome: "JV",
+// //   sobrenome: "C. de Lima",
+// //   idade: 19,
+// //   estaAprovado: true
+// //   };
+// // console.log(objeto1);
+// // console.log(typeof objeto1);
 
-//Licao para casa:
-//Crie um objeto que seja semelhante aos dados do formulario de login, ou seja, com os campos email e senha. Depois imprima o objeto no console.
-//imprima o tipo do objeto criado no console.
+// // //Licao para casa:
+// // //Crie um objeto que seja semelhante aos dados do formulario de login, ou seja, com os campos email e senha. Depois imprima o objeto no console.
+// // //imprima o tipo do objeto criado no console.
 
-const login = {
-    email: "joao@example.com",
-    senha: "123456"
-}; 
-console.log(login);
-console.log(typeof login);
+// //Declarando um objeto espelhado ao FORMULÁRIO DE LOGIN
+
+// //Objeto Usuário:
+
+// const usuario = {
+//     email: "email@example.com",
+//     senha: "123456",
+//     name:"João Vitor",
+//     idade:19,
+//     avatar:"./img/avatar/img-avatar-01.png"
+// }
+
+// //Imprimindo os dados do objeto:
+// console.log(usuario);
+// console.table(usuario);
+
+// //Imprimindo um único dado do objeto:
+// console.log(usuario.nome);
+
+// //Alterar um único dado do objeto:
+// usuario.nome = "Ana Clara";
+// //Imprimindo um único dado do objeto:
+// console.log(usuario.nome);
+
+// let nome = "Guilerme";
+// //Alterar um único dado do objeto:
+// usuario.nome = nome;
+
+// //Imprimindo um único dado do objeto:
+// console.log(usuario.nome);
+
+//const subTitH2 = document.getElementById("teste");
+//Imprimeindo o elemento capturado:
+//console.log(subTitH2);
+
+//Vamos escrever no h2
+// subTitH2.textContent = "<p>NOVO TEXTO</p>";
+//subTitH2.innerHTML = "<p>novo texto</p>";
+
+// const usuario = {
+//      email: "email@example.com",
+//      senha: "123456",
+//      name:"João Vitor",
+//      idade:19,
+//      avatar:"./img/avatar/img-avatar-01.png"
+// }
+
+// //Capturando o elemento de avatar:
+// const imgAvatar = document.getElementById("avatar-user")
+
+// //Preenchendo o avatar com os dados do objeto usuário:
+// imgAvatar.src = usuario.avatar;
+// imgAvatar.width = 100;
+// imgAvatar.alt = usuario.nome;
+
+//Recuperar os dados dos campos do FORM e armazenar em um objeto e imprimir eles assim que o botão Entrar for clicado!!
